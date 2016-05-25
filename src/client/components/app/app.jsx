3f158c1ca470d72c0097675fb6ceb506ff9f3b27
John <jomcode@gmail.com>
2016-05-25 00:23:49 -0500

    rxjs subscription set up to sync app component with StateService

--- src/client/components/app/app.jsx
+++ src/client/components/app/app.jsx
@@ -9,19 +9,41 @@ class App extends Component {
     this.state = {
       stocks: {}
     };
+
+    this.stateSubscription = Feathers.stateService.state$.subscribe(
+      (s) => {
+        const updatedStocks = Object.assign({}, this.state.stocks, s);
+        console.log('updatedStocks', updatedStocks);
+
+        const updatedState = Object.assign({}, this.state, { stocks: updatedStocks });
+
+        this.setState(updatedState);
+      }
+    );
   }
 
   componentDidMount() {
+    const { stocks } = this.state;
     Feathers.synchronize()
-      .then(() => this.setState({ stocks: Feathers.stateService.getState() }));
-    // Feathers.getStock('FB')
-      // .then(() => this.setState({ stocks: Feathers.stateService.getState() }));
+    /* Get Data For Development Purposes */
+      .then(() => {
+        if (Object.keys(stocks).length < 1) {
+          Feathers.getStock('FB');
+          Feathers.getStock('TOL');
+        }
+      });
+  }
+
+  componentWillUnmount() {
+    this.stateSubscription.unsubscribe();
   }
 
   render() {
+    const { stocks } = this.state;
+
     return (
       <div>
-        <p>number of stocks: {Object.keys(this.state.stocks).length}</p>
+        <p>number of stocks: {Object.keys(stocks).length}</p>
       </div>
     );
   }
--- src/client/services/state.js
+++ src/client/services/state.js
@@ -1,8 +1,17 @@
+import { Subject } from 'rxjs/Subject';
+
 function StateService() {
   this.state = {};
+
+  this.stateSource = new Subject();
+  this.state$ = this.stateSource.asObservable();
 }
 
-StateService.prototype.reset = function reset() { this.state = {}; };
+StateService.prototype.reset = function reset() {
+  const updatedState = {};
+  this.state = updatedState;
+  this.pushToStateStream(updatedState);
+};
 
 StateService.prototype.setStock = function setStock(d) {
   const stockId = Object.keys(d)[0];
@@ -10,6 +19,7 @@ StateService.prototype.setStock = function setStock(d) {
 
   const updatedState = Object.assign({}, this.state, d);
   this.state = updatedState;
+  this.pushToStateStream(updatedState);
 };
 
 StateService.prototype.removeStock = function removeStock(d) {
@@ -17,14 +27,21 @@ StateService.prototype.removeStock = function removeStock(d) {
   const updatedState = Object.assign({}, this.state);
   delete updatedState[d];
   this.state = updatedState;
+  this.pushToStateStream(updatedState);
 };
 
 StateService.prototype.sync = function sync(s) {
-  this.state = Object.assign({}, s);
+  const updatedState = Object.assign({}, s);
+  this.state = updatedState;
+  this.pushToStateStream(updatedState);
 };
 
 StateService.prototype.getState = function getState() {
   return Object.assign({}, this.state);
+};
+
+StateService.prototype.pushToStateStream = function pushToStateStream(s) {
+  this.stateSource.next(s);
 };
 
 const service = new StateService();
